1f500526bac6baaeb898f6665e461e88f22b06e0
Wlad <wlad.c@me.com>
2023-05-01 12:56:59 +0200

    change suggested by gpt4

--- autogpt/memory_management/summary_memory.py
+++ autogpt/memory_management/summary_memory.py
@@ -13,32 +13,15 @@ def get_newly_trimmed_messages(
     current_context: List[Dict[str, str]],
     last_memory_index: int,
 ) -> Tuple[List[Dict[str, str]], int]:
-    """
-    This function returns a list of dictionaries contained in full_message_history
-    with an index higher than prev_index that are absent from current_context.
-
-    Args:
-        full_message_history (list): A list of dictionaries representing the full message history.
-        current_context (list): A list of dictionaries representing the current context.
-        last_memory_index (int): An integer representing the previous index.
-
-    Returns:
-        list: A list of dictionaries that are in full_message_history with an index higher than last_memory_index and absent from current_context.
-        int: The new index value for use in the next loop.
-    """
-    # Select messages in full_message_history with an index higher than last_memory_index
-    # do a deep copy to avoid modifying the original list
     new_messages = [
-        copy.deepcopy(msg)
+        copy.copy(msg)
         for i, msg in enumerate(full_message_history)
         if i > last_memory_index
     ]
-    # Remove messages that are already present in current_context
     new_messages_not_in_context = [
         msg for msg in new_messages if msg not in current_context
     ]
 
-    # Find the index of the last message processed
     new_index = last_memory_index
     if new_messages_not_in_context:
         last_message = new_messages_not_in_context[-1]
@@ -48,39 +31,19 @@ def get_newly_trimmed_messages(
 
 
 def update_running_summary(current_memory: str, new_events: List[Dict]) -> str:
-    """
-    This function takes a list of dictionaries representing new events and combines them with the current summary,
-    focusing on key and potentially important information to remember. The updated summary is returned in a message
-    formatted in the 1st person past tense.
-
-    Args:
-        new_events (List[Dict]): A list of dictionaries containing the latest events to be added to the summary.
-
-    Returns:
-        str: A message containing the updated summary of actions, formatted in the 1st person past tense.
-
-    Example:
-        new_events = [{"event": "entered the kitchen."}, {"event": "found a scrawled note with the number 7"}]
-        update_running_summary(new_events)
-        # Returns: "This reminds you of these events from your past: \nI entered the kitchen and found a scrawled note saying 7."
-    """
-    # Replace "assistant" with "you". This produces much better first person past tense results.
     for event in new_events:
         if event["role"].lower() == "assistant":
             event["role"] = "you"
-            # Remove "thoughts" dictionary from "content"
             content_dict = json.loads(event["content"])
             if "thoughts" in content_dict:
                 del content_dict["thoughts"]
             event["content"] = json.dumps(content_dict)
         elif event["role"].lower() == "system":
             event["role"] = "your computer"
-        # Delete all user messages
-        elif event["role"] == "user":
-            new_events.remove(event)
 
-    # This can happen at any point during execturion, not just the beginning
-    if len(new_events) == 0:
+    new_events = [event for event in new_events if event["role"] != "user"]
+
+    if not new_events:
         new_events = "Nothing new happened."
 
     prompt = f'''Your task is to create a concise running summary of actions and information results in the provided text, focusing on key and potentially important information to remember.
